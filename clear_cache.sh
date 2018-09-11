89a8b7bffc3289a1f178f8a55ffd8480d443c8fb
Goran Ninkovic <g.ninkovic@beeit.rs>
2018-09-11 15:27:16 +0200

    added varnish, zsh, elasticsearch, nodejs10 and some more helping scripts

---
+++ clear_cache.sh
@@ -0,0 +1,8 @@
+rm -rf src/var/cache/*
+echo "Deleted var/cache/";
+rm -rf src/var/generated/*
+echo "Deleted var/generated/";
+rm -rf src/var/page_cache/*
+echo "Deleted var/page_cache/";
+rm -rf src/var/view_preprocessed/*
+echo "Deleted var/view_preprocessed/\n";
